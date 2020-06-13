09369a486dcc5161634181ae6b147b52857d4191
Ethan Yang <ethan.y@outlook.com>
2020-06-13 16:16:55 +1000

    Add HelloWorld Project.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using McMathLib;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var math = new MathClass();
+            var number = math.Calculate(1, 2, "Mul");
+            Console.WriteLine(number);
+        }
+    }
+}
