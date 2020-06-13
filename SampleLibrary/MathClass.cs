91a51d769c7410f5a088ef5629bd9ab40493a289
Ethan Yang <ethan.y@outlook.com>
2020-06-13 16:00:42 +1000

    Add MathClass

---
+++ SampleLibrary/MathClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace McMathLib
+{
+    public class MathClass
+    {
+        public MathClass() { }
+
+        public double Calculate(double num1, double num2, string op)
+        {
+            double result = 0;
+            switch (op)
+            {
+                case "Add":
+                    result = num1 + num2;
+                    break;
+                case "Sub":
+                    result = num1 + num2;
+                    break;
+                case "Mul":
+                    result = num1 * num2;
+                    break;
+                case "Div":
+                    result = (double)num1 / num2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
